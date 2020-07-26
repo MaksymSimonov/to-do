7f52c1eee779f6cc8777537d8aba7df9e375b39f
MaksymSimonov <simonovmax90@gmail.com>
2020-07-26 21:40:11 +0300

    removed versionKey from the card schema

--- models/card.js
+++ models/card.js
@@ -6,7 +6,7 @@ const Card = new Schema(
     title: String,
     tasks: [{ task: String, done: Boolean, date: Number }],
     date: Number,
-  }
+  }, { versionKey: false }
 )
 
 module.exports = mongoose.model('cards', Card)
